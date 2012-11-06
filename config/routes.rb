f192c4cbcec99df810efad7fac9defb58c72530b
terry heath <theath@gmail.com>
2012-11-06 15:00:20 -0600

    better styling; default route

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Votebang::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'ballots#new'
 
   # See how all your routes lay out with "rake routes"
 
